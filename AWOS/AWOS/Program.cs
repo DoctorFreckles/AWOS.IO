52481ce1e916cb26fa75d70c7931fa8a24c598cd
Daniel <yo@ientj.com>
2018-11-10 11:33:48 -0800

    Starting encryption utility project.

---
+++ AWOS/AWOS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace AWOS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        public static void SimpleEncrypt(string plainText, string cypherTextFileName)
+        {
+
+        }
+        public static string SimpleDecrypt(string cypherTextFileName)
+        {
+            return null;
+        }
+        public static string GetSimpleKey()
+        {
+            return null;
+        }
+    }
+}
